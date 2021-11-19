6fbabf869ed4bbc7dc3325641d207ddb8866092d
ShixuanZhao <shixuanzhao0608@gmail.com>
2021-11-18 22:44:27 -0800

    init project

---
+++ starlink/starlink/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                2021 StarLink Tracker. Made by ShixuanZhao. All Rights Reserved.
+            </footer>
+        );
+    }
+}
+
+export default Footer;
